057ed835308f136c5cfcdc448dad576e68993c1f
maheshwari1234 <68109585+maheshwari1234@users.noreply.github.com>
2020-07-29 15:44:41 +0530

    Using react hook

--- to_send/src/components/newPost.js
+++ to_send/src/components/newPost.js
@@ -1,76 +1,65 @@
-import React from 'react'
-import { Button, Form, Navbar, Nav,Image,Card} from 'react-bootstrap'
+import React,{useState} from 'react'
+import { Button, Form, Navbar, Nav, Image, Card } from 'react-bootstrap'
 import '../App.css';
-import {NewpostAction} from '../Redux/action/newPostAction'
-import {connect} from 'react-redux'
+import { NewpostAction } from '../Redux/action/newPostAction'
+import { useSelector, useDispatch } from 'react-redux';
 
-class NewPost extends React.Component {
-    constructor(props){
-        super(props)
-        this.state={
-            title:"",
-            body:"",
-            course:"",
-            image:"",
-            Brief:"",
-            submitted:false
-        }
-    }
+const NewPost=(props)=>{
 
-    handleChange = (e) => {
-        const name = e.target.name
-        const value = e.target.value
-        this.setState({ ...this.state, [name]: value } )
-    }
+    const [inputs, setInputs] = useState({});
+    const newDataSubmitted= useSelector((state) => state.newpost.data)
+    const dispatch = useDispatch();
 
-    submit=(e)=>{
+    const handleChange = (event) => {
+        event.persist();
+        setInputs(inputs => ({ ...inputs, [event.target.name]: event.target.value }));
+    
+      }
+
+      const submit=(e)=>{
         e.preventDefault();
 
-    const newData={
-        
-        "Course":this.state.course,
-        "Title":this.state.title,
-        "Brief":this.state.Brief,
-        "Body":this.state.body,
-        "Image":"comman.jpg"
-        
-    }
+        const newData = {
 
-    this.props.newData(newData)
-
-    }
-
-    
-
-    render() {
-        // const post=this.state
-        if(this.state.submitted){
-            this.props.history.push("/posts")
+            "Course": inputs.course,
+            "Title": inputs.title,
+            "Brief": inputs.Brief,
+            "Body": inputs.body,
+            "Image": "comman.jpg"
 
         }
-        return (
-            <React.Fragment>
+        dispatch(NewpostAction(newData))
+
+
+      }
+      if(newDataSubmitted){
+        props.history.push("/")
+      }
+
+    return(
+        <React.Fragment>
                 <Navbar bg="light" variant="dark">
                     <Navbar.Brand href="/home">
-                        <Image src="./logo192.png" style={{ width: 100, marginTop: -7, height: 50 }}  alt="Image not found"/>
+                        <Image src="./logo192.png" style={{ width: 100, marginTop: -7, height: 50 }} alt="Image not found" />
                     </Navbar.Brand>
                     <Nav className="mr-auto">
+                        {/* <Nav.Link style={{ color: "black", fontSize: "20px" }} href="/home">Home</Nav.Link> */}
                         <Nav.Link style={{ color: "black", fontSize: "20px" }}>Write a new post</Nav.Link>
                     </Nav>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
-                            <Nav.Item><Nav.Link href="/new" style={{ color: "black" }}>Edit</Nav.Link></Nav.Item>
+                            <Nav.Item><Nav.Link href="/home" style={{ color: "black" }}>Home</Nav.Link></Nav.Item>
                         </Nav>
                     </Navbar.Collapse>
-                </Navbar>
+                </Navbar><br />
                 <div className="row">
                     <div className="offset-2 col-sm-6">
                         <Card>
                             <Card.Header>
                                 <Form>
-                                <Form.Label style={{ fontWeight: "bold", fontSize: "20px"}}>Add Course Name here..</Form.Label>
+                                    <Form.Label style={{ fontWeight: "bold", fontSize: "20px" }}>Add Course Name here..</Form.Label>
 
-                                <Form.Control as="textarea" type="text" rows="1" onChange={this.handleChange} name="course"/>
+                                    <Form.Control as="textarea" type="text" rows="1" onChange={handleChange} name="course" />
 
                                 </Form>
                             </Card.Header>
@@ -78,53 +67,38 @@ class NewPost extends React.Component {
                                 <Form style={{ marginLeft: "20px" }}>
                                     <Form.Group>
                                         <Form.Label style={{ fontWeight: "bold", fontSize: "40px", color: "gray" }}>New post title here...</Form.Label>
-                                        <Form.Control as="textarea" type="text" rows="2" onChange={this.handleChange} name="title"/>
+                                        <Form.Control as="textarea" type="text" rows="2" onChange={handleChange} name="title" />
                                     </Form.Group>
 
                                     <Form.Group>
                                         <Form.Label style={{ fontWeight: "bold", fontSize: "30px", color: "gray" }}>Brief about new post here..</Form.Label>
-                                        <Form.Control as="textarea" type="text" rows="2" onChange={this.handleChange} name="Brief"/>
+                                        <Form.Control as="textarea" type="text" rows="2" onChange={handleChange} name="Brief" />
                                     </Form.Group>
 
                                     <Form.File
                                         label="Upload Image"
-                                        custom 
-                                        onChange={this.handleChange} name="image"
+                                        custom
+                                        onChange={handleChange} name="image"
                                     />
                                 </Form>
                             </Card.Title>
                             <Card.Body>
                                 <Form>
                                     <Form.Group>
-                                        <Form.Label style={{ fontWeight: "bold", fontSize: "20px" }} onChange={this.handleChange} value={this.state.body} name="body" >Write here Post content here..</Form.Label>
-                                        <Form.Control as="textarea" rows="6"  type="text" onChange={this.handleChange} name="body"/>
+                                        <Form.Label style={{ fontWeight: "bold", fontSize: "20px" }} onChange={handleChange} value={inputs.body} name="body" >Write here Post content here..</Form.Label>
+                                        <Form.Control as="textarea" rows="6" type="text" onChange={handleChange} name="body" />
                                     </Form.Group>
                                 </Form>
                             </Card.Body>
                         </Card><br />
-                        <Button variant="primary" onClick={this.submit}>Publish</Button>
+                        <Button variant="primary" onClick={submit}>Publish</Button>
                     </div>
                 </div>
             </React.Fragment>
 
-        )
-    }
-}
-
-const mapStateToProps=(state)=>{
-    console.log("newpost",state.newpost.data)
-return{
-    newData:state.newpost.data
-}
-}
-
-const mapDispatchToProps=(dispatch)=>{
-    return{
-        newData:(data)=>{
-dispatch(NewpostAction(data))
-        }
-    }
+    )
+    
 }
 
 
-export default connect(mapStateToProps,mapDispatchToProps)(NewPost)
+export default NewPost
