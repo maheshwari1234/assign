57e388f6681c0041bf1a99e91f76212d74b765e1
maheshwari1234 <68109585+maheshwari1234@users.noreply.github.com>
2020-07-29 15:42:46 +0530

    Using React Hooks

--- to_send/src/components/Navbar.js
+++ to_send/src/components/Navbar.js
@@ -1,84 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Redirect } from "react-router-dom";
 import { Navbar, Nav, Form, FormControl, Button, NavDropdown } from 'react-bootstrap';
-import { connect } from 'react-redux'
-import { logoutAction } from '../Redux/action/Loginaction'
+import { logoutAction } from '../Redux/action/Loginaction';
+import { useDispatch, useSelector } from 'react-redux';
 
-class Navbarr extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            word: "",
-            clicked: false,
-        }
+console.log("navbar2")
+
+const Navbarr = () => {
+    const [inputs, setInputs] = useState({});
+    const [name,setName]=useState({submitted:false,clicked:false})
+    const [categ,setCat]=useState('')
+
+    const isLogged = useSelector((state) => state.login.isLogged)
+    const isLogRegister = useSelector((state) => state.register.isLogged)
+    const dispatch = useDispatch();
+
+    const handleChange = (event) => {
+        event.persist();
+        setInputs(inputs => ({ ...inputs, [event.target.name]: event.target.value }));
+    }
+    const submit = () => {        
+           setName({submitted:true})
+           
+ 
     }
 
-    handleChange = (e) => {
-        const name = e.target.name
-        const value = e.target.value
-        this.setState({ ...this.state, [name]: value })
+    const category = (cat) => {
+        console.log("cat",cat)
+        setCat(cat)
+        setName({clicked:true})
     }
 
-    submit = () => {
-        this.setState({ clicked: true })
+    if(name.submitted){
+        const word=inputs.word
+        return <Redirect to={`/search/${word}`} />
     }
 
-    logout = () => {
-        this.props.logout()
+    if(name.clicked){
+        console.log("",categ)
+        const cat=categ
+        console.log("cat in click",cat)
+        return <Redirect to={`/t/${cat}`} />
     }
-    render() {
-        if (this.state.clicked) {
-            const word = this.state.word
-            return <Redirect to={`/search/${word}`} />
-        }
-        return (
-            <React.Fragment>
-                <Navbar bg="secondary" variant="dark">
-                    <Form inline>
-                        <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange={this.handleChange} name="word" />
-                        <Button variant="outline-light" onClick={this.submit}>Search</Button>
-                    </Form>
-                    <Nav className="mr-auto">
-                        <NavDropdown title="Posts by category" value={this.state.value}>
-                            <NavDropdown.Item href="/t/React" value="React">React</NavDropdown.Item>
-                            <NavDropdown.Item href="/t/Javascript" value="Javascript">Javascript</NavDropdown.Item>
-                            <NavDropdown.Item href="/t/Bootstrap" value="Bootstrap">Bootstrap</NavDropdown.Item>
 
-                        </NavDropdown>
+    return (
+        <React.Fragment>
+            <Navbar bg="secondary" variant="dark">
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange={handleChange} name="word" />
+                    <Button variant="outline-light" onClick={submit}>Search</Button>
+                </Form>
+                <Nav className="mr-auto">
+                    <NavDropdown title="Posts by category" >
+                        <NavDropdown.Item onClick={() => { category("React") }} value="React">React</NavDropdown.Item>
+                        <NavDropdown.Item onClick={() => { category("Javascript") }} value="Javascript">Javascript</NavDropdown.Item>
+                        <NavDropdown.Item onClick={() => { category("Bootstrap") }} value="Bootstrap">Bootstrap</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        {(isLogged || isLogRegister) ? <Nav.Item><Nav.Link href="/new" style={{ color: "yellow" }}>Write a Post</Nav.Link></Nav.Item> : null}
+                        {(isLogged || isLogRegister) ?
+                            <Button variant="warning" onClick={dispatch(logoutAction())}>Logout</Button> :
+                            <Nav.Item><Nav.Link href="/login" style={{ color: "yellow" }}>Login</Nav.Link></Nav.Item>
+                        }
+
                     </Nav>
-                    <Navbar.Collapse id="basic-navbar-nav">
+                </Navbar.Collapse>
 
-                        <Nav className="ml-auto">
-                            {(this.props.isLogged || this.props.isLogRegister)?<Nav.Item><Nav.Link href="/new" style={{ color: "yellow" }}>Write a Post</Nav.Link></Nav.Item>:null}
-                            
-                            {(!this.props.isLogged || this.props.isLogRegister)?
-                                <Nav.Item><Nav.Link href="/login" style={{ color: "yellow" }}>Login</Nav.Link></Nav.Item> :
-                                <Button variant="warning" onClick={this.logout}>Logout</Button>
-                            }
+            </Navbar>
+        </React.Fragment>
+    )
 
-                        </Nav>
-                    </Navbar.Collapse>
-
-                </Navbar>
-            </React.Fragment>
-        )
-    }
 }
-
-const mapStateToProps = (state) => {
-    console.log("logout",state.login.isLogged)
-    return {
-        isLogged: state.login.isLogged,
-        isLogRegister:state.register.isLogged
-    }
-}
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        logout: () => {
-            dispatch(logoutAction())
-        }
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Navbarr);
+export default Navbarr;
